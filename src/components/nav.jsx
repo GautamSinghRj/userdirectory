93d72d25431ac9dbb42175fdbad502f490b0c5db
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-09 12:55:50 +0530

    new border added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav(){
     return(
-        <div className="container mx-auto p-6 border-2 border-black">
-            <div className="flex flex-row ">
+        <div className="container mx-auto p-6 border border-black">
+            <div className="flex flex-row justify-center items-center gap-10">
             <a href="/">Home</a>
             <a href="/client-posts">Client-Posts</a>
             <a href="/post-by-id">Post-by-Id</a>

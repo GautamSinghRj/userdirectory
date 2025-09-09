34e377a313bfec8c685f432c341adc73e18cf2b9
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-09 12:44:11 +0530

    proper nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,11 @@
 export default function Nav(){
     return(
-        <div className="container mx-auto p-6">
-            <div className="flex flex-row border-2 border-black"></div>
+        <div className="container mx-auto p-6 border-2 border-black">
+            <div className="flex flex-row ">
             <a href="/">Home</a>
             <a href="/client-posts">Client-Posts</a>
             <a href="/post-by-id">Post-by-Id</a>
+            </div>
         </div>
     );
 }

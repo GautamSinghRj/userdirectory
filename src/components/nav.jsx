d95a3fa0d9828419b0cd40b23686fc16baac91da
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-09 13:02:45 +0530

    add anim and dark

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,10 @@
 export default function Nav(){
     return(
-        <div className="container mx-auto p-6 border border-dotted border-black my-4">
+        <div className="container mx-auto p-6 border-2 border-dotted rounded-b-3xl border-black my-4">
             <div className="flex flex-row justify-center items-center gap-10">
-            <a href="/">Home</a>
-            <a href="/client-posts">Client-Posts</a>
-            <a href="/post-by-id">Post-by-Id</a>
+            <a href="/" className="font-bold transition duration-300 ease-in-out hover:underline">Home</a>
+            <a href="/client-posts" className="font-bold transition duration-300 ease-in-out hover:underline">Client-Posts</a>
+            <a href="/post-by-id" className="font-bold transition duration-300 ease-in-out hover:underline">Post-by-Id</a>
             </div>
         </div>
     );

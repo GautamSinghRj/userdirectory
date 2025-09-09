15d9027eac772159fae8094bc8184f2ec548ff43
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-09 13:13:50 +0530

    new changes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,6 @@
 export default function Nav(){
     return(
-        <div className="container mx-auto p-6 border-2 border-dashed rounded-3xl border-black bg-gray-100 my-4">
+        <div className="container mx-auto p-6 border-2 border-dashed rounded-3xl border-slate-400 bg-gray-100 my-4">
             <div className="flex flex-row justify-center items-center gap-10">
             <a href="/" className="font-bold transition duration-75 ease-in-out hover:underline">Home</a>
             <a href="/client-posts" className="font-bold transition duration-75 ease-in-out hover:underline">Client-Posts</a>

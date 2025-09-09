23e41b3d991eb61b85c8f49ae11da0b2c2acbac9
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-09 12:39:05 +0530

    nav Bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,10 @@
 export default function Nav(){
     return(
         <div className="container mx-auto p-6">
-
+            <div className="flex flex-row border-2 border-black"></div>
+            <a href="/">Home</a>
+            <a href="/client-posts">Client-Posts</a>
+            <a href="/post-by-id">Post-by-Id</a>
         </div>
     );
 }

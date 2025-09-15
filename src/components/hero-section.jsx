582c1946e4a8e19e1fea6d5478c06af6a201efb2
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-15 13:30:36 +0530

    some changes

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
     return (
-        <div className="relative h-[750px]">
-            <img src="../assets/hero-section.jpg" alt="Hero section background" className="z-10 w-full h-full object-cover" />
+        <div className="relative h-[750px] mb-10">
+            <img src="/herosection.jpg" alt="Hero section background" className="z-10 w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black/30 z-10"></div>
             <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center z-20 gap-12 mt-72">
                 <p className="drop-shadow-lg font-extrabold text-7xl max-w-7xl">
-                    My Blog. My First Project.</p>
+                    My Blog. My First NextJs Project.</p>
                 <p className="drop-shadow-lg font-extralight text-2xl max-w-4xl leading-relaxed">
                     This is the hero section of my project.
                 </p>

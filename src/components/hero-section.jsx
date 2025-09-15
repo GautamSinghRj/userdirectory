8e19524bfffbb1eed89f10750ad9944fc77edb9a
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-15 13:22:24 +0530

    new

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
     return (
         <div className="relative h-[750px]">
-            <img src="../src/assets/hero-section.jpg" alt="Hero section background" className="z-10 w-full h-full object-cover" />
+            <img src="../assets/hero-section.jpg" alt="Hero section background" className="z-10 w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black/30 z-10"></div>
             <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center z-20 gap-12 mt-72">
                 <p className="drop-shadow-lg font-extrabold text-7xl max-w-7xl">

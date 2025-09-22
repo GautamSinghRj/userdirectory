e3e3ac06f7a0ea43596b69c57ad3f65d1caab6e7
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-22 13:11:38 +0530

    added database support new

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 
-const globalForPrisma = global;
+const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}

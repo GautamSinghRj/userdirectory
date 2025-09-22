dee1587c8c8e92b7d6363e6eadb859bf2615fa55
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-09-22 17:04:02 +0530

    added some changes to scehma prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,6 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis;
-
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
